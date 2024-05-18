f7b564091996ac1bb82415a408761a4fee2c6ece
Wicked Jack <studentwj.p4dhb@passmail.net>
2024-05-18 21:53:43 +0200

    Edited some stylings so that navbar is positioned at top edge of browser screen.

--- environment-website-app/src/App.jsx
+++ environment-website-app/src/App.jsx
@@ -1,9 +1,21 @@
 import './App.css'
+import MainNavbar from '../src/components/navbar/MainNavbar';
+import RainbowHeader from './components/common/RainbowHeader';
 
 function App() {
+  const navbarElements = [
+    { href: '/', text: 'Home' },
+    { href: '/about', text: 'About' },
+    { href: '/services', text: 'Services' },
+    { href: '/contact', text: 'Contact' }
+  ];
+
   return (
     <>
-      <h1>Mitwelt Deutschland</h1>
+      <MainNavbar elements={navbarElements} />
+      <header className="App-header">
+        <RainbowHeader text="Mitwelt Deutschland"></RainbowHeader>
+      </header>
       <div>Willkommen auf der Seite von Mitwelt Deutschland.</div>
     </>
   )
